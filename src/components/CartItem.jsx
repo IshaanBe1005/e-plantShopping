c5c5be73dcf1f062c838801e1f0d06e232290d33
IshaanBe1005 <149373934+IshaanBe1005@users.noreply.github.com>
2026-04-03 15:32:18 +0530

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
-import { increaseQty, decreaseQty, removeFromCart } from "../redux/CartSlice";
+import { updateQuantity, removeItem } from "../redux/CartSlice";
 
 function CartItem({ goHome }) {
   const cart = useSelector(state => state.cart);
   const dispatch = useDispatch();
 
-  const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+  // Total cart amount
+  const totalAmount = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
 
   return (
     <div>
@@ -13,22 +17,47 @@ function CartItem({ goHome }) {
 
       {cart.map(item => (
         <div key={item.id}>
+          <img src={item.img} alt={item.name} />
           <h3>{item.name}</h3>
-          <p>${item.price} x {item.quantity}</p>
+          <p>Unit Price: ${item.price}</p>
+          <p>Quantity: {item.quantity}</p>
           <p>Total: ${item.price * item.quantity}</p>
 
-          <button onClick={() => dispatch(increaseQty(item.id))}>+</button>
-          <button onClick={() => dispatch(decreaseQty(item.id))}>-</button>
-          <button onClick={() => dispatch(removeFromCart(item.id))}>
+          {/* Increase / Decrease */}
+          <button
+            onClick={() =>
+              dispatch(updateQuantity({ id: item.id, type: "increase" }))
+            }
+          >
+            +
+          </button>
+
+          <button
+            onClick={() =>
+              dispatch(updateQuantity({ id: item.id, type: "decrease" }))
+            }
+          >
+            -
+          </button>
+
+          {/* Delete */}
+          <button onClick={() => dispatch(removeItem(item.id))}>
             Delete
           </button>
         </div>
       ))}
 
-      <h3>Total Cart Amount: ${total}</h3>
+      <h3>Total Cart Amount: ${totalAmount}</h3>
 
-      <button onClick={() => alert("Coming Soon")}>Checkout</button>
-      <button onClick={goHome}>Continue Shopping</button>
+      {/* Checkout */}
+      <button onClick={() => alert("Coming Soon")}>
+        Checkout
+      </button>
+
+      {/* Continue Shopping */}
+      <button onClick={goHome}>
+        Continue Shopping
+      </button>
     </div>
   );
 }
